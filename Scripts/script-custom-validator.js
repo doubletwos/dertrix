0da18cb2bbd104f9f44b4a297553803449ff3820
doubletwos <wilsonwales@gmail.com>
2020-11-16 04:52:31 +0000

    0452

--- Scripts/script-custom-validator.js
+++ Scripts/script-custom-validator.js
@@ -124,6 +124,119 @@ $(document).ready(function () {
 	});
 });
 
+$(document).ready(function () {
+	$('#myCreatePostForm').validate({
+		errorClass: 'help-block animation-slideDown', // You can change the animation class for a different entrance animation - check animations page
+		errorElement: 'div',
+		errorPlacement: function (error, e) {
+			e.parents('.form-group > div').append(error);
+		},
+		highlight: function (e) {
+			$(e).closest('.form-group').removeClass('has-success has-error').addClass('has-error');
+			$(e).closest('.help-block').remove();
+		},
+		success: function (e) {
+			e.closest('.form-group').removeClass('has-success has-error');
+			e.closest('.help-block').remove();
+		},
+		rules: {
+			'PostTopicId': {
+				required: true,
+			},
+			'PostSubject': {
+				required: true,
+			},
+			'PostContent': {
+				required: true,
+			}
+		},
+		messages: {
+			'PostTopicId': 'Please select appropriate topic',
+			'PostSubject': 'Please enter subject',
+			'PostContent': 'Post can not be empty'
+		}
+	});
+});
+
+$(document).ready(function () {
+	$('#mySubjectForm').validate({
+		errorClass: 'help-block animation-slideDown', // You can change the animation class for a different entrance animation - check animations page
+		errorElement: 'div',
+		errorPlacement: function (error, e) {
+			e.parents('.form-group > div').append(error);
+		},
+		highlight: function (e) {
+			$(e).closest('.form-group').removeClass('has-success has-error').addClass('has-error');
+			$(e).closest('.help-block').remove();
+		},
+		success: function (e) {
+			e.closest('.form-group').removeClass('has-success has-error');
+			e.closest('.help-block').remove();
+		},
+		rules: {
+			'SubjectName': {
+				required: true,
+				maxlength: 20
+			},
+			'ClassId': {
+				required: true,
+			},
+			'ClassTeacherId': {
+				required: true,
+			}
+		},
+		messages: {
+			'SubjectName': {
+				required: 'Please enter subject name',
+				maxlength: 'Not more than 20 characters allowed'
+			},
+			'ClassId': 'Please select class',
+			'ClassTeacherId': 'Please assign subject to a teacher'
+		}
+	});
+});
+
+$(document).ready(function () {
+	$('#AddNewZeusUserForm').validate({
+		errorClass: 'help-block animation-slideDown', // You can change the animation class for a different entrance animation - check animations page
+		errorElement: 'div',
+		errorPlacement: function (error, e) {
+			e.parents('.form-group > div').append(error);
+		},
+		highlight: function (e) {
+			$(e).closest('.form-group').removeClass('has-success has-error').addClass('has-error');
+			$(e).closest('.help-block').remove();
+		},
+		success: function (e) {
+			e.closest('.form-group').removeClass('has-success has-error');
+			e.closest('.help-block').remove();
+		},
+		rules: {
+			'RegisteredUserTypeId': {
+				required: true,
+			},
+			'IsTester': {
+				required: true,
+			},
+			'FirstName': {
+				required: true,
+			},
+			'LastName': {
+				required: true,
+			},
+			'Email': {
+				required: true,
+			}
+		},
+		messages: {
+			'RegisteredUserTypeId': 'Please select user type',
+			'IsTester': 'Is user a tester?',
+			'FirstName': 'Please enter first name',
+			'LastName': 'Please enter last name',
+			'Email': 'Please enter email address',
+		}
+	});
+});
 
 
 
