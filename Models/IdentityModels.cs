9bd80ed8440106937d559d1b3bfd22d668b71b36
doubletwos <wilsonwales@gmail.com>
2021-07-09 20:54:48 +0100

    205409072021

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -50,8 +50,6 @@ namespace Dertrix.Models
         public DbSet<RegUsersAccessLog> RegUsersAccessLogs { get; set; }
         public DbSet<Relationship> Relationships { get; set; }
         public DbSet<Title> Titles { get; set; }
-        public DbSet<OrgImportantDate> OrgImportantDates { get; set; }
-        public DbSet<OrgEvent> OrgEvents { get; set; }
         public DbSet<Org_Events_Log> Org_Events_Logs { get; set; }
         public DbSet<NurserySchoolUserRole> NurserySchoolUserRoles { get; set; }
         public DbSet<OrgSchCalendar> OrgSchCalendars { get; set; }
